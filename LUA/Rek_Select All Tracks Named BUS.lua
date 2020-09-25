77c7423ebf81b1887ab66d0ab3db1672495ceb30
Rekonin <chrisdnorris@gmail.com>
2020-09-25 22:20:58 +0100

    Update Rek_Nudge Item Volume +6dB.eel, Rek_Nudge Item Volume -6dB.eel, and Rek_Select All Tracks Named BUS.lua

--- LUA/Rek_Select All Tracks Named BUS.lua
+++ LUA/Rek_Select All Tracks Named BUS.lua
@@ -1,19 +1,14 @@
-local name = 'BUS'
-local tracks = reaper.CountTracks()
-
-if not tracks == 0 then bla() return end
-
 reaper.PreventUIRefresh(1)
 reaper.Undo_BeginBlock()
 
 reaper.Main_OnCommand(40297,0) --Track: Unselect all tracks
 
-for i = 0, tracks-1 do
-  local tr = reaper.GetTrack(0, i)
-  local _, tr_name = reaper.GetSetMediaTrackInfo_String(tr, 'P_NAME', 'BUS', 0)
-  if tr_name == name then reaper.SetTrackSelected(tr, 1)
-  end;
-end;
+for i = 1, reaper.CountSelectedTracks(0) do
+  tr = reaper.GetSelectedTrack(0,i-1)
+  _, tr_name = reaper.GetSetMediaTrackInfo_String(tr, 'P_NAME', '', false)
+  tr_name_last_sym = string.upper(string.sub(tr_name,-3))
+  if tr_name_last_sym == 'BUS' then reaper.SetTrackSelected(tr, 1) end
+end
 
-reaper.Undo_EndBlock('Select track with BUS in name', -1)
+reaper.Undo_EndBlock('Select All Tracks with BUS in Name', -1)
 reaper.PreventUIRefresh(-1)
