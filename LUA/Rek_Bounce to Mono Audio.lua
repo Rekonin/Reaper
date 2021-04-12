dae9910c6ae8caa58ef64b51d462f23e86eb627f
Chris <chrisdnorris@gmail.com>
2021-04-12 20:56:55 +0100

    Update

--- LUA/Rek_Bounce to Mono Audio.lua
+++ LUA/Rek_Bounce to Mono Audio.lua
@@ -4,7 +4,7 @@ reaper.Undo_BeginBlock()
 reaper.Main_OnCommand(40361, 0) --Item: Apply track/take FX to items (mono output)
 reaper.Main_OnCommand(40698, 0) --Edit: Copy items
 reaper.Main_OnCommand(40318, 0) --Item navigation: Move cursor left to edge of item
-reaper.Main_OnCommand(40285, 0) --Track: Go to next track
+reaper.Main_OnCommand(40001, 0) --Track: Insert new track
 reaper.Main_OnCommand(42398, 0) --Item: Paste items/tracks
 reaper.Main_OnCommand(reaper.NamedCommandLookup('_XENAKIOS_SELECTFIRSTTAKEOFITEMS'), 0)
 reaper.Main_OnCommand(40129, 0) --Take: Delete active take from items
@@ -13,7 +13,7 @@ reaper.Main_OnCommand(reaper.NamedCommandLookup('_XENAKIOS_SELECTLASTTAKEOFITEMS
 reaper.Main_OnCommand(40129, 0) --Take: Delete active take from items
 reaper.Main_OnCommand(40719, 0) --Item properties: Mute
 
-reaper.Undo_EndBlock('Commit MIDI to Mono Audio to Next Track', 0)
+reaper.Undo_EndBlock('Bounce to Mono Audio', 0)
 reaper.PreventUIRefresh(-1)
 reaper.UpdateArrange()
 reaper.UpdateTimeline()
