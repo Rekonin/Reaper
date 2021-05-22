595dbb7d655b6dee20be240b37f0f65f9d2294bf
Chris <chrisdnorris@gmail.com>
2021-05-22 01:05:40 +0100

    Create Buss - Update 2

--- LUA/Rek_Create Buss for Selected Tracks.lua
+++ LUA/Rek_Create Buss for Selected Tracks.lua
@@ -22,18 +22,8 @@ reaper.SetTrackSendInfo_Value(tr, 0, send, 'D_VOL', 1)
 reaper.SetTrackSendInfo_Value(tr, 0, send, 'I_SENDMODE', 0)
 end
 
---reaper.Main_OnCommand(reaper.NamedCommandLookup('_SWS_TRACKRANDCOL'), 0)
-
-local HasExtState = reaper.HasExtState('Color BUS', 'COLOR_BUS');
-if HasExtState then;
-reaper.DeleteExtState('Color BUS', 'COLOR_BUS', false);
-end;
-local Color = reaper.GetMediaTrackInfo_Value(first_sel, 'I_CUSTOMCOLOR');
-reaper.SetExtState('Color BUS', 'COLOR_BUS', Color, false);
-
 reaper.Main_OnCommand(reaper.NamedCommandLookup('_XENAKIOS_SELFIRSTOFSELTRAX'), 0)
 reaper.Main_OnCommand(reaper.NamedCommandLookup('_XENAKIOS_SELPREVTRACK'), 0)
---reaper.SetMediaTrackInfo_Value(bus, 'I_CUSTOMCOLOR', Color);
 reaper.Main_OnCommand(reaper.NamedCommandLookup('_XENAKIOS_INSTRACKLABSUFF'), 0)
 reaper.Main_OnCommand(reaper.NamedCommandLookup('_SWSAUTOCOLOR_APPLY'), 0)
 
