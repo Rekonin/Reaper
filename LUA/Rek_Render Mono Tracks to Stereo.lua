dbfc36c656bb136db3d3763731e79a226ac60d3c
Chris <chrisdnorris@gmail.com>
2022-04-24 15:07:31 +0100

    Update Rek_Render Mono Tracks to Stereo.lua
    Fixed script for multiple items on tracks

--- LUA/Rek_Render Mono Tracks to Stereo.lua
+++ LUA/Rek_Render Mono Tracks to Stereo.lua
@@ -2,7 +2,7 @@ reaper.PreventUIRefresh(1)
 reaper.Undo_BeginBlock()
 
 reaper.Main_OnCommand(reaper.NamedCommandLookup("_XENAKIOS_SELNEXTTRACKKEEP"), 0)
-reaper.Main_OnCommand(reaper.NamedCommandLookup("_XENAKIOS_SELFIRSTITEMSOFTRACKS"), 0)
+reaper.Main_OnCommand(40718, 0) --Item: Select all items on selected tracks in current time selection
 reaper.Main_OnCommand(reaper.NamedCommandLookup("_XENAKIOS_IMPLODEITEMSPANSYMMETRICALLY"), 0)
 reaper.Main_OnCommand(reaper.NamedCommandLookup("_XENAKIOS_SELFIRSTOFSELTRAX"), 0)
 reaper.Main_OnCommand(40788, 0) --Track: Render tracks to stereo stem tracks (and mute originals)
