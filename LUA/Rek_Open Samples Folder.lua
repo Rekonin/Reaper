e576c576996b5ee3d6cd842c748fa48b57c06e30
Chris <chrisdnorris@gmail.com>
2023-02-26 22:29:43 +0000

    REEEEEEFRESH!

--- LUA/Rek_Open Samples Folder.lua
+++ LUA/Rek_Open Samples Folder.lua
@@ -1,6 +1,6 @@
 local function No_Undo()end; local function no_undo()reaper.defer(No_Undo)end;
 
-local pathFile = '"G:\\SAMPLES"'
+local pathFile = '"D:\\SAMPLES"'
 
 if reaper.GetOS() == "OSX32" or reaper.GetOS() == "OSX64" then;
     os.execute('open "'..pathFile..'"');
